d46c7fd784069eabeb5f4c4fd4fd6080f6abc27b
Mina SaȜd <73399220+Mina-Saad-2022@users.noreply.github.com>
2024-11-08 23:24:51 +0200

    UseRef

--- src/components/introductions/Hooks/UseRef/index.js
+++ src/components/introductions/Hooks/UseRef/index.js
@@ -1,6 +1,39 @@
-const function_UseRef = () => {
-    return "UseRef";
+import React, { useRef } from 'react';
+import { Button , Form } from "react-bootstrap";
+
+const SimpleUseRefExample = () => {
+  // هنا بنعمل متغير باستخدام useRef عشان نمسك بيه ال input
+  const inputRef = useRef(null);
+
+  // هنستخدم الدالة دي عشان نحط التركيز على input لما نضغط على الزرار
+  const focusInput = () => {
+    inputRef.current.focus(); // .current بترجع لنا العنصر اللي بنشاور عليه
   };
-  
-  export { function_UseRef };
-  
+
+  return (
+    <div>
+      <Form.Control className='w-50' type="text" ref={inputRef} placeholder="Type here..." />
+      <Button className='m-2' variant="success" onClick={focusInput}>Focus on Input</Button> {/* عند الضغط هيركز علي input */}
+    </div>
+  );
+};
+
+export default SimpleUseRefExample;
+
+const function_UseRef = () => {
+  return (
+    <>
+      <div className="container">
+        <h2>UseRef</h2>
+        <ul>
+          <li>
+         يعني مرجع والغرض منها انها بتخليني اشاور علي اي عنصر واخد القيمة بتاعته referance هي اختصار لكلمة ref كلمة 
+          </li>
+        </ul>
+      </div>
+      <SimpleUseRefExample />
+    </>
+  );
+};
+
+export { function_UseRef };
