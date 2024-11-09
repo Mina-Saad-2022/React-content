c31fe6e2d0f69f784f1394ad085262bf3e56a465
Mina SaȜd <73399220+Mina-Saad-2022@users.noreply.github.com>
2024-11-09 19:49:26 +0200

    some update in usecontext

--- src/components/introductions/Hooks/UseContext/index.js
+++ src/components/introductions/Hooks/UseContext/index.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+
+import React, { createContext } from "react";
+
 import { Image, Container, Row, Col } from "react-bootstrap";
 
 const function_UseContext = () => {
@@ -11,51 +13,72 @@ const function_UseContext = () => {
             <p className="text-danger">اللازم هنشتغل بيها hooks من اهم ال </p>
           </Col>
           <Col xs={12} md={8} lg={12}>
-          <ul>
-            <li>
-              انه بيخزن قيم usecontext استخدام ال
-              <br />
-              وكمان علشان يحل المشاكل الممكن تحصل في المشاريع الكبيرة
-              <br />
-              login & logout زي مثلا المشاريع الفيها
-              <br />
-              علشان اعرف هو دخل ولا لا true & flase فمش من المنطقي اني كل شوية
-              هروح امرر قيم
-              <br />
-            </li>
-            <li>
-              {" "}
-              واقدر اوصله من اي مكان داخل المشروعdatabase هخزن قيمة في مكان
-              منفصل كانه في ال usecontext فمن خلال ال
-            </li>
-            <li>contextApi لازم الاول نعرف ال usecontext علشان اشتغل بال</li>
-            <li>
-              contextApi والهو هيكون context من نوع object ببساطة انها بتاخد
-              usecontext وتعريف ال
-              <br />
-              ولكن بخزن قيمة في مكان منفصل state تعتبر عبارة عن usecontext وال
-            </li>
-
-            <li>
-              بيسمح بمشاركة قيم في المشروع react هي عبارة عن جزء من ال
-              contextApi ال
-              <br />
-              مابين data وده بيساعد في حل مشكلة نقل ال components دون الحاجة الي
-              تمريرها بشكل يدوي الي اكثر من
-              <br />
-              prop-driling المشروع والبتحصل من خلال ال components ال
-            </li>
-            <li>
-              <h6 className="text-danger">
-                الخلاصة يعني في اي مكان بيكون عندي زي مخزن صغير وهيكون
+            <ul>
+              <li>
+                انه بيخزن قيم usecontext استخدام ال
                 <br />
-                usecontext وبخزن فيه قيم معينة وعلشان اوصل للقيم دي لازم استخدام
-                ال contextApi هو ال
-              </h6>
-            </li>
-          </ul>
+                وكمان علشان يحل المشاكل الممكن تحصل في المشاريع الكبيرة
+                <br />
+                login & logout زي مثلا المشاريع الفيها
+                <br />
+                علشان اعرف هو دخل ولا لا true & flase فمش من المنطقي اني كل شوية
+                هروح امرر قيم
+                <br />
+              </li>
+              <li>
+                {" "}
+                واقدر اوصله من اي مكان داخل المشروعdatabase هخزن قيمة في مكان
+                منفصل كانه في ال usecontext فمن خلال ال
+              </li>
+              <li>contextApi لازم الاول نعرف ال usecontext علشان اشتغل بال</li>
+              <li>
+                contextApi والهو هيكون context من نوع object ببساطة انها بتاخد
+                usecontext وتعريف ال
+                <br />
+                ولكن بخزن قيمة في مكان منفصل state تعتبر عبارة عن usecontext وال
+              </li>
+
+              <li>
+                بيسمح بمشاركة قيم في المشروع react هي عبارة عن جزء من ال
+                contextApi ال
+                <br />
+                مابين data وده بيساعد في حل مشكلة نقل ال components دون الحاجة
+                الي تمريرها بشكل يدوي الي اكثر من
+                <br />
+                prop-driling المشروع والبتحصل من خلال ال components ال
+              </li>
+              <li>
+                <h6 className="text-danger">
+                  الخلاصة يعني في اي مكان بيكون عندي زي مخزن صغير وهيكون
+                  <br />
+                  usecontext وبخزن فيه قيم معينة وعلشان اوصل للقيم دي لازم
+                  استخدام ال contextApi هو ال
+                </h6>
+              </li>
+            </ul>
           </Col>
-          
+          <Col xs={12} md={8} lg={12}>
+            <h3>طريقة الاستخدام</h3>
+            <ul>
+              <ol>
+                <li>
+                  contextApi لازم اول شئ اعمل ال
+                  <ul>
+                    <ol>
+                      <li>
+                        والمن خلالها هخزن القيم بتاعتي import React عند ال context هنشأ في
+                        الاول ال
+                        <Image
+                className="w-75 zoom-image mt-2 rounded"
+                src="https://i.ibb.co/x2Pvf1d/image.png"
+              />
+                      </li>
+                    </ol>
+                  </ul>
+                </li>
+              </ol>
+            </ul>
+          </Col>
         </Row>
       </Container>
     </>
