e6be63fe18e6822217244b69db5040b538bce126
Mina-Saad-2022 <mina.saad2255@gmail.com>
2024-12-02 14:59:37 +0200

    hover heart in card

--- src/Projects/Menu/Body.js
+++ src/Projects/Menu/Body.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Card, Button } from "react-bootstrap";
 
 const Body = ({ data }) => {
@@ -6,25 +6,43 @@ const Body = ({ data }) => {
     <Row>
       {data.map((item) => (
         <Col className="mb-2" xl={4} key={item.id}>
-          <Card>
-            <Card.Img className="image_card" variant="top" src={item.photo} />
-            <Card.Body>
-              <Card.Title className="text-center">{item.name}</Card.Title>
-              <Card.Text>
-                <h5 className="text-danger">Category: {item.category}</h5>
-              </Card.Text>
-              <Card.Text>{item.description}</Card.Text>
-              <Card.Text>
-                <h5 className="text-danger">price: {item.price}$ </h5>
-              </Card.Text>
-              <Button className="w-100" variant="primary">
-                Go somewhere
-              </Button>
-            </Card.Body>
-          </Card>
+          <CardComponent item={item} />
         </Col>
       ))}
     </Row>
+  );
+};
+
+const CardComponent = ({ item }) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
+  return (
+    <Card className="card-container">
+      <Card.Img className="image_card" variant="top" src={item.photo} />
+      <div
+        className={`heart-icon ${isLiked ? "liked" : ""}`}
+        onClick={toggleLike}
+      >
+        <i class="fa-solid fa-heart"></i>
+      </div>
+      <Card.Body>
+        <Card.Title className="text-center">{item.name}</Card.Title>
+        <Card.Text>
+          <h5 className="text-danger">Category: {item.category}</h5>
+        </Card.Text>
+        <Card.Text>{item.description}</Card.Text>
+        <Card.Text>
+          <h5 className="text-danger">Price: {item.price}$</h5>
+        </Card.Text>
+        <Button className="w-100" variant="primary">
+          Go somewhere
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
